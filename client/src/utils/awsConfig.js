6a417723a49974db85b47b11b9d180309727961d
the-capital-hub <116173060+MuhamedShahabasC@users.noreply.github.com>
2023-11-29 16:41:27 +0530

    Fixed aws-sdk global issue

--- client/src/utils/awsConfig.js
+++ client/src/utils/awsConfig.js
@@ -1,10 +1,9 @@
-// const AWS = require("aws-sdk");
-import AWS from 'aws-sdk';
+// import AWS from 'aws-sdk';
 
-AWS.config.update({
-  accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY,
-  secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
-  region: "us-east-1",
-});
+// // AWS.config.update({
+// //   accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY,
+// //   secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
+// //   region: "us-east-1",
+// // });
 
-export const s3 = new AWS.S3();
+// export const s3 = new AWS.S3();
